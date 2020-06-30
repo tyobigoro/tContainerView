b532d6b31445df1dba70a90ee50578558c72a40d
tyobigoro_i <tyobigorox@gmail.com>
2020-07-01 03:53:42 +0900

    Initial Commit

---
+++ tContainerView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tContainerView
+//
+//  Created by tyobigoro on 2020/07/01.
+//  Copyright © 2020 tyobigoro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
